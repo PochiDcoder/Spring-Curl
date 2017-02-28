fee6a76bedbb58e7020917b07e8c4ae30a92fbce
PochiDcoder <prashanth.pochi@gmail.com>
2017-02-27 23:40:28 -0500

    cURL with spring-mongodb

---
+++ gsaccessingmongodbdatarest/src/main/java/hello/Application.java
@@ -0,0 +1,13 @@
+package hello;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+/**
+ * Created by prashanth pochiraju on 2/27/2017.
+ */
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
